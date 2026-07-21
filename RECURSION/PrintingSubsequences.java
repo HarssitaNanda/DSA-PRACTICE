01ecba3f70d61c0c30c3788d95718e1d77f3a92e
HarssitaNanda <nandaharssita@gmail.com>
2026-07-21 22:20:51 +0530

    Printing Subsequences recursively.

---
+++ RECURSION/PrintingSubsequences.java
@@ -0,0 +1,191 @@
+
+import java.util.ArrayList;
+
+class PrintingSubseuqences{
+    public static void main(String[] args) {
+        int[] arr={3,1,2};
+        ArrayList<Integer> set=new ArrayList<>();
+        subsequences(arr,0,set);     
+    }
+    static void subsequences(int[] arr,int i,ArrayList<Integer> set){
+        if(i>=arr.length){
+            System.out.println(set.toString());
+            return;
+        }
+
+        set.add(arr[i]);
+        subsequences(arr, i+1, set);
+        set.remove(set.size() - 1);
+        subsequences(arr, i+1, set);
+
+
+    }
+
+}
+/*
+Mistakes that I did:
+
+### 1. Wrong base condition
+
+Your base condition is:
+
+```java
+if(i==arr.length-1)
+```
+
+This stops before considering the last element.
+
+It should be:
+
+```java
+if(i == arr.length){
+    System.out.println(set);
+    return;
+}
+```
+
+---
+
+### 2. Wrong way to remove from ArrayList
+
+You wrote:
+
+```java
+set.remove(arr[i]);
+```
+
+`ArrayList` has two `remove()` methods:
+
+```java
+remove(int index)
+remove(Object o)
+```
+
+Since `arr[i]` is an `int`, Java thinks you're calling
+
+```java
+remove(index)
+```
+
+For your array
+
+```java
+{3,1,2}
+```
+
+when `arr[i]` is `3`, Java tries
+
+```java
+set.remove(3);
+```
+
+which means **remove the element at index 3**, not the value `3`.
+
+This causes an `IndexOutOfBoundsException`.
+
+What you actually want is to remove the **last element you just added**:
+
+```java
+set.remove(set.size() - 1);
+```
+
+This is called **backtracking**.
+
+---
+
+## Correct Code
+
+```java
+import java.util.ArrayList;
+
+class PrintingSubsequences {
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 2};
+        ArrayList<Integer> set = new ArrayList<>();
+        subsequences(arr, 0, set);
+    }
+
+    static void subsequences(int[] arr, int i, ArrayList<Integer> set) {
+
+        if (i == arr.length) {
+            System.out.println(set);
+            return;
+        }
+
+        // Take
+        set.add(arr[i]);
+        subsequences(arr, i + 1, set);
+
+        // Don't Take
+        set.remove(set.size() - 1);
+        subsequences(arr, i + 1, set);
+    }
+}
+```
+
+---
+
+### Output
+
+```
+[3, 1, 2]
+[3, 1]
+[3, 2]
+[3]
+[1, 2]
+[1]
+[2]
+[]
+```
+
+---
+
+## Why remove the last element?
+
+Suppose:
+
+```
+set = []
+```
+
+Take `3`
+
+```
+set = [3]
+```
+
+Take `1`
+
+```
+set = [3,1]
+```
+
+Take `2`
+
+```
+set = [3,1,2]
+```
+
+Print.
+
+Now you're returning to explore the branch where `2` is **not** taken.
+
+So first undo the previous choice:
+
+```
+set.remove(set.size()-1)
+
+set = [3,1]
+```
+
+Then recurse again.
+
+Every recursive call **adds one element**, so every return must **remove that same last-added element**. This is the essence of **backtracking**.
+
+---
+
+### A small challenge for you
+
+Without looking it up, can you explain why the base condition must be `i == arr.length` instead of `i == arr.length - 1`? Thinking through what happens to the last element will help you understand recursion more deeply.
+
+ */
